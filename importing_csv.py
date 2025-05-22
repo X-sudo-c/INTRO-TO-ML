593d7551e6d66e6e3ce39a5270bd2dcc33e4d1f5
X-sudo-c <x1dragonn@gmail.com>
2025-05-22 23:56:46 +0000

    describin the data

--- importing_csv.py
+++ importing_csv.py
@@ -5,3 +5,9 @@ data_path = "/home/x-x/Documents/INTRO-TO-ML/melb_data.csv"
 data = pd.read_csv(data_path)
 
 print(data.describe())
+
+print(data.columns)
+
+# print(data.dropna(axis=0))
+
+# print(data.price)
